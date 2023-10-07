e0be37bbf1355df744779ce1a90ae3f9bf7e551d
MNITCR <108656979+MNITCR@users.noreply.github.com>
2023-10-07 17:24:00 +0700

    cm

--- js/test.js
+++ js/test.js
@@ -77,60 +77,237 @@ $(document).ready(function() {
 // ======================nav right=====================
 $(document).ready(function() {
     // Click event to icon
-    $('.flex-right-nav li').click(function() {
-        $('.flex-medium-nav svg path').attr('fill', '#606266');
-        // Reset styles for all list items
-        $('.flex-right-nav li').removeClass('selected');
-        $('.flex-right-nav li div').css('color', '#050505');
-        $('.flex-right-nav li svg path').attr('fill', '#050505');
+    // $('.flex-right-nav li').click(function() {
+    //     $('.flex-medium-nav svg path').attr('fill', '#606266');
+    //     // Reset styles for all list items
+    //     $('.flex-right-nav li').removeClass('selected');
+    //     $('.flex-right-nav li div').css('color', '#050505');
+    //     $('.flex-right-nav li svg path').attr('fill', '#050505');
 
-        // Update styles for the clicked list item
-        $(this).addClass('selected');
-        $(this).find('div').css('color', '#bad8f2');
-        $(this).find('svg path').attr('fill', '#0866FF');
-    });
+    //     // Update styles for the clicked list item
+    //     $(this).addClass('selected');
+    //     $(this).find('div').css('color', '#bad8f2');
+    //     $(this).find('svg path').attr('fill', '#0866FF');
+    // });
 
-    // =====================Main-Dropdown-Menu-Nav-Right==================
-    var showAndHideDropdownMenu = false;
-    $('.Menu-Main').click(() => {
-        $('.Dropdown-Message-Nav-Right').css('display', 'none');
-        if(showAndHideDropdownMenu == false){
-            $('.Main-Dropdown-Menu-Nav-Right').css('display', 'block');
-            // alert('Dropdown');
-            showAndHideDropdownMenu = true;
+    // // =====================Main-Dropdown-Menu-Nav-Right==================
+    // var showAndHideDropdownMenu = false;
+    // $('.Menu-Main').click(() => {
+    //     $('.Dropdown-Message-Nav-Right').css('display', 'none');
 
-        }else{
-            $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
-            // alert('Dropdown 12312');
-            showAndHideDropdownMenu = false;
+    //     if (!showAndHideDropdownMenu) {
+    //         $('.Main-Dropdown-Menu-Nav-Right').css('display', 'block');
+    //         showAndHideDropdownMenu = true;
 
-            // Click event to icon
-            $('.flex-medium-nav svg path').attr('fill', '#606266');
-            $('.flex-right-nav li svg path').attr('fill', '#050505');
-        }
+    //         // Update styles for the clicked list item
+    //         $('.Menu-Messenger svg path').attr('fill', '');
+    //         $('.Menu-Main').addClass('selected');
+    //         $('.Menu-Main').find('div').css('color', '#bad8f2');
+    //         $('.Menu-Main').find('svg path').attr('fill', '#0866FF');
+    //         alert('hi');
+    //     } else {
+    //         $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+    //         showAndHideDropdownMenu = false;
 
-    });
-    // =====================End Main-Dropdown-Menu-Nav-Right==================
+    //         $('.Menu-Main svg path').attr('fill', '');
+    //         $('.Menu-Messenger svg path').attr('fill', '');
+    //         $('.Menu-Main').removeClass('selected');
+    //         $('.Menu-Main div').css('color', '#050505');
+    //         $('.Menu-Main svg path').attr('fill', '#050505');
+    //     }
+    // });
+    // // =====================End Main-Dropdown-Menu-Nav-Right==================
 
-    // =====================Main-Dropdown-Message-Nav-Right==================
-    var showAndHideDropdownMessage = false;
-    $('.Menu-Messenger').click(() => {
-        $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
-        if(showAndHideDropdownMessage == false){
-            $('.Dropdown-Message-Nav-Right').css('display', 'block');
-            showAndHideDropdownMessage = true;
+    // // =====================Main-Dropdown-Message-Nav-Right==================
+    // var showAndHideDropdownMessage = false;
+    // $('.Menu-Messenger').click(() => {
+    //     $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+    //     if(!showAndHideDropdownMessage){
+    //         $('.Dropdown-Message-Nav-Right').css('display', 'block');
+    //         showAndHideDropdownMessage = true;
 
-        }else{
-            $('.Dropdown-Message-Nav-Right').css('display', 'none');
-            showAndHideDropdownMessage = false;
+    //         $('.Menu-Main svg path').attr('fill', '');
+    //         $('.Menu-Messenger').addClass('selected');
+    //         $('.Menu-Messenger').find('div').css('color', '#bad8f2');
+    //         $('.Menu-Messenger').find('svg path').attr('fill', '#0866FF');
+    //     }else{
+    //         $('.Dropdown-Message-Nav-Right').css('display', 'none');
+    //         showAndHideDropdownMessage = false;
 
-            // Click event to icon
-            $('.flex-medium-nav svg path').attr('fill', '#606266');
-            $('.flex-right-nav li svg path').attr('fill', '#050505');
-        }
-    });
-
+    //         $('.Menu-Main svg path').attr('fill', '');
+    //         $('.Menu-Messenger svg path').attr('fill', '');
+    //         $('.Menu-Messenger').removeClass('selected');
+    //         $('.Menu-Messenger div').css('color', '#050505');
+    //         $('.Menu-Messenger svg path').attr('fill', '#050505');
+    //     }
+    // });
 
     // =====================End Main-Dropdown-Message-Nav-Right==================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // $(document).ready(() =>{
+
+    //     function toggleListItem($element, $otherElement, $dropdown) {
+    //         $element.toggleClass('selected');
+    //         $element.find('div').css('color', $element.hasClass('selected') ? '#bad8f2' : '#050505');
+    //         $element.find('svg path').attr('fill', $element.hasClass('selected') ? '#0866FF' : '#050505');
+
+    //         // Hide the other menu
+    //         $otherElement.removeClass('selected');
+    //         $otherElement.find('div').css('color', '#050505');
+    //         $otherElement.find('svg path').attr('fill', '#050505');
+
+    //         // Toggle visibility of dropdown
+    //         $dropdown.toggle();
+    //     }
+
+    //     $('.Main-Create').click(() => {
+    //         toggleListItem($('.Main-Create'), $('.Menu-Messenger', '.Menu-Main', '.Main-notifications', '.Main-Your-profile'), $('.Dropdown-ADD-Nav-Right'));
+    //         $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-Message-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-notifications-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-Profile-Nav-Right').css('display', 'none');
+    //     });
+
+    //     $('.Menu-Main').click(() => {
+    //         toggleListItem($('.Menu-Main'), $('.Menu-Messenger','.Menu-Create', '.Main-notifications', '.Main-Your-profile'), $('.Main-Dropdown-Menu-Nav-Right'));
+    //         $('.Dropdown-Message-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-ADD-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-notifications-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-Profile-Nav-Right').css('display', 'none');
+    //     });
+
+    //     $('.Menu-Messenger').click(() => {
+    //         toggleListItem($('.Menu-Messenger'), $('.Menu-Main', '.Main-Create', '.Main-notifications', '.Main-Your-profile'), $('.Dropdown-Message-Nav-Right'));
+    //         $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-ADD-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-notifications-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-Profile-Nav-Right').css('display', 'none');
+    //     });
+
+    //     $('.Main-notifications').click(() => {
+    //         toggleListItem($('.Main-notifications'), $('.Menu-Main', '.Main-Create', '.Menu-Messenger', '.Main-Your-profile'), $('.Dropdown-notifications-Nav-Right'));
+    //         $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-Profile-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-ADD-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-Message-Nav-Right').css('display', 'none');
+    //     });
+
+
+    //     $('.Main-Your-profile').click(() => {
+    //         toggleListItem($('.Main-Your-profile'), $('.Menu-Main', '.Main-Create', '.Menu-Messenger', '.Main-notifications'), $('.Dropdown-Profile-Nav-Right'));
+    //         $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-ADD-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-Message-Nav-Right').css('display', 'none');
+    //         $('.Dropdown-notifications-Nav-Right').css('display', 'none');
+    //     });
+    // });
+
+
+    $(document).ready(() =>{
+
+        function toggleListItem($element, $otherElement, $dropdown) {
+            $element.toggleClass('selected');
+            $element.find('div').css('color', $element.hasClass('selected') ? '#bad8f2' : '#050505');
+            $element.find('svg path').attr('fill', $element.hasClass('selected') ? '#0866FF' : '#050505');
+
+            // Handle color change for other elements
+            $otherElement.removeClass('selected');
+            $otherElement.find('div').css('color', '#050505');
+            $otherElement.find('svg path').attr('fill', '#050505');
+
+            // Toggle visibility of dropdown
+            $dropdown.toggle();
+        }
+
+        $('.Main-Create').click(() => {
+            toggleListItem($('.Main-Create'), $('.Menu-Messenger, .Menu-Main, .Main-notifications, .Main-Your-profile'), $('.Dropdown-ADD-Nav-Right'));
+            $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+            $('.Dropdown-Message-Nav-Right').css('display', 'none');
+            $('.Dropdown-notifications-Nav-Right').css('display', 'none');
+            $('.Dropdown-Profile-Nav-Right').css('display', 'none');
+        });
+
+        $('.Menu-Main').click(() => {
+            toggleListItem($('.Menu-Main'), $('.Menu-Messenger, .Main-Create, .Main-notifications, .Main-Your-profile'), $('.Main-Dropdown-Menu-Nav-Right'));
+            $('.Dropdown-Message-Nav-Right').css('display', 'none');
+            $('.Dropdown-ADD-Nav-Right').css('display', 'none');
+            $('.Dropdown-notifications-Nav-Right').css('display', 'none');
+            $('.Dropdown-Profile-Nav-Right').css('display', 'none');
+        });
+
+        $('.Menu-Messenger').click(() => {
+            toggleListItem($('.Menu-Messenger'), $('.Menu-Main, .Main-Create, .Main-notifications, .Main-Your-profile'), $('.Dropdown-Message-Nav-Right'));
+            $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+            $('.Dropdown-ADD-Nav-Right').css('display', 'none');
+            $('.Dropdown-notifications-Nav-Right').css('display', 'none');
+            $('.Dropdown-Profile-Nav-Right').css('display', 'none');
+        });
+
+        $('.Main-notifications').click(() => {
+            toggleListItem($('.Main-notifications'), $('.Menu-Main, .Main-Create, .Menu-Messenger, .Main-Your-profile'), $('.Dropdown-notifications-Nav-Right'));
+            $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+            $('.Dropdown-Profile-Nav-Right').css('display', 'none');
+            $('.Dropdown-ADD-Nav-Right').css('display', 'none');
+            $('.Dropdown-Message-Nav-Right').css('display', 'none');
+        });
+
+        $('.Main-Your-profile').click(() => {
+            toggleListItem($('.Main-Your-profile'), $('.Menu-Main, .Main-Create, .Menu-Messenger, .Main-notifications'), $('.Dropdown-Profile-Nav-Right'));
+            $('.Main-Dropdown-Menu-Nav-Right').css('display', 'none');
+            $('.Dropdown-ADD-Nav-Right').css('display', 'none');
+            $('.Dropdown-Message-Nav-Right').css('display', 'none');
+            $('.Dropdown-notifications-Nav-Right').css('display', 'none');
+        });
+    });
+
+
+
+
 });
 // =====================end nav right=====================
